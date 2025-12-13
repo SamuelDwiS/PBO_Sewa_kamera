cce6441cfa4c27d0902d3603f38c57c248643aa0
SamDwis <140613647+SamuelDwiS@users.noreply.github.com>
2025-12-13 21:43:37 +0700

    Delete PBO_Sewa_kamera/PBO_Sewa_kamera/PBO_Sewa_kamera-main directory

--- PBO_Sewa_kamera/PBO_Sewa_kamera/PBO_Sewa_kamera-main/admin/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-// Hapus semua session
-session_unset();
-session_destroy();
-// Redirect ke halaman login
-header("Location: ../page/login.php?msg=logout");
-exit;
--- PBO_Sewa_kamera/PBO_Sewa_kamera/PBO_Sewa_kamera-main/page/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Logout user
-if (!isset($_SESSION)) session_start();
-
-session_unset();
-session_destroy();
-
-header('Location: login.php');
-exit;
